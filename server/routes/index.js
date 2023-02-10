7a699c43641d96dde7922b808951ca3fff9e21a3
aram20134 <dasigty@gmail.com>
2023-02-10 12:09:25 +0300

    ++

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,11 @@
 const Router = require('express')
 const router = new Router()
 const userRouter = require('./userRouter')
+const projectRouter = require('./projectRouter')
+
 
 router.use('/user', userRouter)
+router.use('/project', projectRouter)
 
 module.exports = router
+
